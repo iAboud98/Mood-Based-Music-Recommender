b2fea03c76c831cc18cc34428fc32e612e2fdc3b
iAboud98 <aboudfialah98@gmail.com>
2025-01-23 23:09:41 +0200

    add more sentiment options, handle edge cases

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,14 +1,24 @@
 from textblob import TextBlob
 
-text = input()  # -> Temporarily created before taking mood from prompt
+text = input("Type your current mood -> ")  # -> Temporarily created before taking mood from prompt
+
+if not text.strip():  # -> if user didn't type anything
+    print("Please enter a valid response.")
+    exit()
 
 current_mood = TextBlob(text).polarity  # -> analyze current mood from text
 
-mood = "Natural"
+mood = "Natural"  # -> initial mood
 
-if current_mood > 0:
-    mood = "Happy"
-elif current_mood < 0:
-    mood = "Sad"
+# -> update based on input text from user
+if current_mood > 0.5:
+    mood = "Very Happy"
+elif 0 < current_mood <= 0.5:
+    mood = "Slightly Happy"
+elif 0 > current_mood >= -0.5:
+    mood = "Slightly Sad"
+elif -0.5 > current_mood >= -1:
+    mood = "Very Sad"
 
+print(current_mood)
 print(mood)
